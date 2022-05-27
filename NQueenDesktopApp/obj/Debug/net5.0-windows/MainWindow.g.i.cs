bcb2ec58761b6ba2bdda52672319438d6948e14f
Oleg <oleg.dragoi228@gmail.com>
2022-05-28 01:16:40 +0200

    Added previously deleted file
    Deleted NQueenDesktopApp.exe by accident

--- NQueenDesktopApp/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ NQueenDesktopApp/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -125,7 +125,7 @@ namespace NQueenDesktopApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NQueenDesktopApp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NQueenDesktopApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
